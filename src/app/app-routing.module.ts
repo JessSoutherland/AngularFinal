272291b696d25865c18e108f149e2a9a55ae551e
JessSoutherland <66761446+JessSoutherland@users.noreply.github.com>
2020-10-01 19:44:27 -0400

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskAddComponent } from './task-add/task-add.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path: "", redirectTo:"list", pathMatch: "full"},
+  {path: "list", component: TaskListComponent},
+  {path: "add", component: TaskAddComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskAddComponent } from './task-add/task-add.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskAddComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, HttpClientModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
